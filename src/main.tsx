dcaf80a3883496d7d72543ac33846a73ed043b72
bekodani <bekodani@gmail.com>
2026-04-20 10:36:48 +0200

    fix router import

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./AppLayout";
+import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
