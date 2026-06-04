06bc9d42dd3779fbf0429c6d22c4e753bd6bf08f
bekodani <bekodani@gmail.com>
2026-06-04 18:04:14 +0200

    add pollster interface

---
+++ shared/types/pollsters.ts
@@ -0,0 +1,18 @@
+export interface Pollster {
+    id: string;
+    label: string;
+    description?: string;
+    errorMargin: {
+        min: number;
+        max: number;
+    };
+    bias?: PollBias[];
+}
+
+export type PollBias =
+    | ResultBias;
+
+export interface ResultBias {
+    type: 'result';
+    partyBias: Record<string, number>;
+}
