9c9e03065b5197bcb4734271fcb8d30cb404fc6d
bekodani <bekodani@gmail.com>
2026-08-06 07:55:20 +0200

    fix: correct syntax error in deadlock result configuration

--- apps/kampanykorut/src/logic/application/hooks/useElectionState.test.ts
+++ apps/kampanykorut/src/logic/application/hooks/useElectionState.test.ts
@@ -73,7 +73,7 @@ const mockConfig: CampaignConfig = {
             title: "Deadlock",
             subtitle: "",
             description: "",
-          }
+          },
         },
       },
     },
