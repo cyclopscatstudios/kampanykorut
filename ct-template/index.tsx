721986fe43313b03d4768324285f00bd292562c9
bekodani <bekodani@gmail.com>
2026-08-08 22:24:42 +0200

    fix: update import path for i18n logic in index.tsx

--- ct-template/index.tsx
+++ ct-template/index.tsx
@@ -1,4 +1,4 @@
 import "reflect-metadata";
-import "../src/logic/i18n/i18n";
+import "../apps/kampanykorut/src/logic/i18n/i18n";
 // Import styles, initialize component theme here.
 // import '../src/common.css';
