b249c7a7de64419f48cb9f3b1cf465e29ba1342b
bekodani <bekodani@gmail.com>
2026-08-10 07:45:43 +0200

    fix format

--- apps/kampanykorut/src/logic/application/loadCampaignConfig.ts
+++ apps/kampanykorut/src/logic/application/loadCampaignConfig.ts
@@ -16,7 +16,7 @@ export function loadCampaignConfig(
 ): Promise<CampaignConfig> {
   const cached = cache.get(campaignId);
   if (cached) {
-    console.log("cached config loaded", {cached});
+    console.log("cached config loaded", { cached });
     return cached;
   }
 
